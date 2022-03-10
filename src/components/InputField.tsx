50ed9e60296fbbc3c36b64db52a02c89de28f28c
Franco Traverso <franco-t20@outlook.com>
2022-03-09 22:18:55 -0300

    Add submit input style

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,7 +5,7 @@ const InputField = () => {
   return (
     <form className="input">
       <input type="input" placeholder="Enter a task" className="input__box" />
-      <button className="input__submit" type="submit">
+      <button className="input_submit" type="submit">
         Go
       </button>
     </form>

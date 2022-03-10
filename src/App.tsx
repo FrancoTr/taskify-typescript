ba5d07a9c763d929c01a3d54dd9c1509ab905334
Franco Traverso <franco-t20@outlook.com>
2022-03-09 21:43:14 -0300

    Add basic App style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">TS</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+    </div>
+  );
+};
 
 export default App;

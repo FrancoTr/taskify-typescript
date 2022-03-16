71f3475a66ad76b8658157d2359dadc9fd172f10
Franco Traverso <franco-t20@outlook.com>
2022-03-15 21:19:32 -0300

    Render each To to task in TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,7 +8,13 @@ interface Props {
 }
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
-  return <div className="todos">TodoList</div>;
+  return (
+    <div className="todos">
+      {todos.map((todo) => (
+        <li>{todo.todo}</li>
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;

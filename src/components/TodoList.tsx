16aa7a84d1e3a3b7ec337797b958003058f0e107
Franco Traverso <franco-t20@outlook.com>
2022-03-17 19:35:04 -0300

    Add SingleTodo component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Todo } from "../model";
 import "./styles.css";
-
+import SingleTodo from "./SingleTodo";
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
@@ -11,7 +11,7 @@ const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
     <div className="todos">
       {todos.map((todo) => (
-        <li>{todo.todo}</li>
+        <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );

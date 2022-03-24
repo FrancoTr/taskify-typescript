83b4b692723220ad741cc9aeed82eb72141c83d0
Franco Traverso <franco-t20@outlook.com>
2022-03-23 22:21:51 -0300

    Add handleDone onClick function

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import "./styles.css";
+import TodoList from "./TodoList";
 
 type Props = {
   todo: Todo;
@@ -10,6 +11,9 @@ type Props = {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
+  };
   return (
     <form className="todos__single">
       <span className="todos__single--text">{todo.todo}</span>
@@ -20,7 +24,7 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         <span className="icon">
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>

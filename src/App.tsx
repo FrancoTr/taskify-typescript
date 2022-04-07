be8a5882d58dc2e3db6e3726b29b654f20e56f08
Franco Traverso <franco-t20@outlook.com>
2022-04-07 19:32:18 -0300

    Add completedTodos state

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Todo } from "./model";
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault(); //stops the default page refresh after submit
@@ -20,7 +21,12 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        completedTodos={completedTodos}
+        setCompletedTodos={setCompletedTodos}
+      />
     </div>
   );
 };
